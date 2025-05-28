c0f7eff41064857c15816e77e57208a89f4242dd
Israel-Oluwasegun1 <marksegman@outlook.com>
2025-05-28 22:02:37 +0100

    modify multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,3 @@
 number = int(input("Enter a number to see its multiplication table: "))
-for index, num in enumerate(range(number, number*11, number)):
-    print(f"{number} * {index +1} = {num}")
+for mul in range(1, 11):
+    print(f"{number} * {mul} = {number * mul}")
