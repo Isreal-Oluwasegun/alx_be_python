b892317bdbbb1ae334da606ff0111003b9dc73e6
Israel Oluwasegun <marksegman@outlook.com>
2025-06-06 16:03:28 +0100

    add arithmetic_operation.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,16 @@
 def perform_operation(num1, num2, operation):
     result = 0
-    match operation:
-        case 'add':
-            result = num1 + num2
-        case 'subtract':
-            result = num1 - num2
-        case 'multiply':
-            result = num1 * num2
-        case 'divide' if num2 == 0:
-            result = 'Cannot divide by zero'
-        case 'divide':
-            result = num1/num2
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide' and num2 == 0:
+        result = 'Cannot divide by zero'
+    elif operation == 'divide':
+        result = num1/num2
     return f'Result: {result}'
 
-print(perform_operation(20, 10, 'subtract'))
+print(perform_operation(20, 0, 'divide'))
         
