e08f37e273f265ca73c8e16112b3b9a5fee9fb69
Israel Oluwasegun <marksegman@outlook.com>
2025-06-07 12:59:39 +0100

    add temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,11 +3,11 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
   temp = (fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
-  print(f'{fahrenheit}\N{DEGREE SIGN}F is {temp}\N{DEGREE SIGN}C')
+  return f'{fahrenheit}\N{DEGREE SIGN}F is {temp}\N{DEGREE SIGN}C'
 
 def convert_to_fahrenheit(celsius):
   temp = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-  print(f'{celsius}\N{DEGREE SIGN}C is {temp}\N{DEGREE SIGN}F')
+  return f'{celsius}\N{DEGREE SIGN}C is {temp}\N{DEGREE SIGN}F'
 
 
 
@@ -17,7 +17,7 @@ try:
 except:
      print("Invalid temperature. Please enter a numeric value.")
 else:
-    options = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+    options = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
     if options == "C":
         print(convert_to_fahrenheit(temperature))
     elif options == "F":
