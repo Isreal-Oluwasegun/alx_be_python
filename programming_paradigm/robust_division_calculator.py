3e8eb572b8fb53d105d59eeef546db394fc6f82a
Israel Oluwasegun <marksegman@outlook.com>
2025-06-13 20:22:20 +0100

    modify robost division

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,8 +2,8 @@ def safe_divide(numerator, denominator):
     try:
         result = numerator / denominator
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero."
+        print("Error: Cannot divide by zero.")
     except TypeError:
-        return "Error: Please enter numeric values only."
+        print("Error: Please enter numeric values only.")
     else:
-        return "The result of the division is {result}"
+        print("The result of the division is {result}")
