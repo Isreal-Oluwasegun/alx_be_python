2d9c918ac782fbc71af94577869edc957fa736de
Israel Oluwasegun <marksegman@outlook.com>
2025-06-14 00:29:27 +0100

    modify function name

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,7 +4,7 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalsulator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(3,5),8)
         self.assertEqual(self.calc.add(-3, 5), 2)
         self.assertEqual(self.calc.add(5,-3), 2)
@@ -19,7 +19,7 @@ class TestSimpleCalsulator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0,4), 0)
         self.assertEqual(self.calc.multiply(4, 0), 0)
         self.assertNotEqual(self.calc.multiply(4, 3), "444")
-    def test_divide(self):
+    def test_division(self):
         self.assertIsNone(self.calc.divide(3, 0))
         self.assertEqual(self.calc.divide(0,3),0)
         self.assertEqual(self.calc.divide(3,2), 1.5)
