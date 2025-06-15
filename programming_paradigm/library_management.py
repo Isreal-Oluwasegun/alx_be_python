02a9a1063dfa3a883d9757409a4cdfb867188a74
Israel Oluwasegun <marksegman@outlook.com>
2025-06-15 14:23:01 +0100

    add library management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,20 +1,26 @@
 class Book:
-    def __init__(self, title, author):
-        self.title = title               
-        self.author = author             
-        self._is_checked_out = False     
+    def __init__(self, title="unknown", author="unknown"):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
 
-    def check_out(self):
-        if not self._is_checked_out:
+class Library(Book):
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, books):
+        self._books.append(books)
+
+    def list_available_books(self):
+       return self._books
+    
+    def check_out_book(self, title):
+        if title in self._books:
+            self._books.remove(title)
             self._is_checked_out = True
-            return True
-        return False
+        return self._is_checked_out
 
-    def return_book(self):
-        if self._is_checked_out:
+    def return_book(self, title):
+        if self._is_checked_out = True:
+            self._books.append(title)
             self._is_checked_out = False
-            return True
-        return False
-
-    def is_available(self):
-        return not self._is_checked_out
